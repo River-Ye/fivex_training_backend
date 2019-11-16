6711f175c887ed7258f7fc0e588c1cc87299bfc1
River <river800111@gmail.com>
2019-11-16 19:24:24 +0800

    Modify unit test from before to let

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Task, type: :model do
+RSpec.describe Task, type: :model do  
   it "is accessible" do
     task = Task.create
     expect(task).to eq Task.last
