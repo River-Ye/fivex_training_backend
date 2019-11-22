d72a0e7602ff0931ed795049a6a8c17c5e356e1f
River-Ye <river800111@gmail.com>
2019-11-22 11:11:12 +0800

    Add http_basic_authenticate_with

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV["http_auth_username"], password: ENV["http_auth_password"] if Rails.env.production?
 end
